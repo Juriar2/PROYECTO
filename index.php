a7188f3398fb9dd4b7e99fa59de45fa2b3a69391
Juriar2 <yuriar2000@hotmail.com>
2022-06-20 17:49:47 -0500

    versio 1.0 cambios en alguna carpetas 20/06/2022

--- index.php
+++ index.php
@@ -8,58 +8,45 @@
     <meta name="keywords" content="Itss">
     <meta name="author" content="Itss">
     <meta name="theme-color" content="#9E8010">
+    <link rel="stylesheet" href="asset/css/normalize.css">
     <link rel="stylesheet" href="asset/css/inciar_sesion.css">
-    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    
 
     <title>ITSS</title>
 </head>
 <body>
-    <header>
-        <div class="home">
-            <a class="login__home" href="Login">
-               Iniciar sesion
-            </a>
-        </div>
+<header class="hero">
+        <nav class="nav container">
+            <div class="nav__logo" >
+                <h2 class="nav__title"><a href="https://abiyasoftware.com.mx/">ITSS.</a></h2>
+            </div>
+                
+            <ul class="nav__link nav__link--menu">
+                <li class="nav__items">
+                    <a href="login" class="nav__links" target="_new">Iniciar sesion</a>
+                </li>
+                <ul class="nav__link nav__link--menu">
+                <li class="nav__items">
+                    <a href="login" class="nav__links" target="_new">Registrate</a>
+                </li>
+                
+                <img src="./img/close.svg" class="nav__close" alt="img">
+            </ul>
+
+            <div class="nav__menu">
+                <img src="./img/menu.svg" class="nav__img" alt="img">
+            </div>
+        </nav>
+
+        <section class="hero__container container">
+            <h1 class="hero__title slideLeft" >Somo una empresa dedica al creacion de software</h1>
+            <p class="hero__paragraph slideUp">La innovación es lo que distingue a un líder de los demás</p>
+            <a href="Planes" class="cta  wiggle">Mas informacion</a>
+        </section>
     </header>
 
-    <section id="container-slider">
-      <a href="javascript: fntExecuteSlide('prev');" class="arrowPrev"><i class="fas fa-chevron-circle-left"></i></a>
-      <a href="javascript: fntExecuteSlide('next');" class="arrowNext"><i class="fas fa-chevron-circle-right"></i></a>
-      <ul class="listslider">
-        <li><a itlist="itList_0" href="#" class="item-select-slid"></a></li>
-        <li><a itlist="itList_1" href="#"></a></li>
-        <li><a itlist="itList_2" href="#"></a></li>
-      </ul>
-      <ul id="slider">
-        <li style="background-image: url('asset/images/screen.png'); z-index:0; opacity: 1;">
-          <div class="content_slider">
-            <div>
-              <h2>Desarrollador frontend</h2>
-              <p>Soy un estudiante Universitario</p>
-             
-            </div>
-          </div>
-        </li>
-        <li class="imagen01" style="background-image: url('asset/images/bg-1.jpg'); ">
-          <div  class="content_slider">
-            <div>
-              <h2></h2>
-              <p></p>
-
-            </div>
-          </div>
-        </li>
-        <li class="imagen02" style="background-image: url('asset/images/itss.jpg'); ">
-          <div class="content_slider">
-            <div>
-              <h2></h2>
-              <p</p>
-
-            </div>
-          </div>
-        </li>
-      </ul>
-    </section> 
   <script src="asset/js/mover.js"></script>
     
 </body>
