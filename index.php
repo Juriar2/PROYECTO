b0bf36541576cb7a1bdafdd18a50e7ec42470c87
juriar2 <yuriar2000@hotmail.com>
2022-07-05 17:53:10 -0500

    version beta 1.4 se agego un cuenta regresriva aÃl index y estilo al arcivo 05/07/2022

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         <div class="container">
             <nav class="nav d-flex">
                 <a href="#" class="logo">ITSS</a>
-                <a href="login" target="new" class="btn rollerRight">inciar sesion</a></li>
+                <a href="login" target="new" class="btn rollerRight">inciar sesion</a>
                     
             </nav>
         </div>
@@ -35,20 +35,28 @@
             
             <div class="home-wrapper d-flex">
                 <div class="col-left">
-                    <h1 class="slideRight">Protect 
-                        The one You Love</h1>
-                    <p>choose suitable life insurance plans <br>
-                        as per your need</p>
-                    <ul>
-                        <li><img src="assets/images/health.svg" alt="health">Health</li>
-                        <li><img src="assets/images/car.svg" alt="car">Car</li>
-                        <li><img src="assets/images/home.svg" alt="Home">Home</li>
-                    </ul>
-                    <div class="btn-section d-flex">
-                        <a href="#" class="btn-start">Get started</a>
-                        <a href="#" class="btn-play"> <span><img src="assets/images/play.svg" alt="play"></span>
-                            Watch Video</a>
-                    </div>
+                    <h1 class="slideRight">Resgistrate por tiempo ilimitado</h1>
+                        <div class="launch-time">
+                            <div>
+                                <p id="days">00</p>
+                                <span>Dias</span>
+                            </div>
+                            <div>
+                                <p id="hours">00</p>
+                                <span>Horas</span>
+                            </div>
+                            <div>
+                                <p id="minutes">00</p>
+                                <span>Minutos</span>
+                            </div>
+                            <div>
+                                <p id="seconds">00</p>
+                                <span>Segundo</span>
+                            </div>
+                            
+                        </div>
+                        <br>
+                        <a href="Resgistrate" target="new" class="btnresgistrate rollerRight">Inscribete</a>
                 </div>
                 <div class="home-image">
                     <img  src="https://firebasestorage.googleapis.com/v0/b/yuriar-d684c.appspot.com/o/undraw_laravel_and_vue_-59-tp.svg?alt=media&token=1a09f9c0-8ce7-4e66-af5e-a5db6c672b65" class="pulse">
@@ -60,5 +68,7 @@
         <br>
         <img class="imagenes " src="https://firebasestorage.googleapis.com/v0/b/yuriar-d684c.appspot.com/o/curva3.svg?alt=media&token=4f9257f2-ee11-4bdc-a648-bbce44fee233">
     </section>
+    <script src="asset/js/tiempo.js"></script>
+
 </body>
 </html>
