bd19c78aa4e62690cfe6f586ebfd0ed4e2901794
juriar2 <yuriar2000@hotmail.com>
2022-07-07 13:56:25 -0500

    cambios vesion 1.6 07/07/2022

--- view/AdminMntUsuario/modalmantenimiento.php
+++ view/AdminMntUsuario/modalmantenimiento.php
@@ -12,43 +12,43 @@
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Nombre: <span class="tx-danger">*</span></label>
-                            <input class="form" id="usu_nom" type="text" name="usu_nom"  placeholder="Ingrese el nombre ejemplo maria" />
-                            <i class="formulario__validacion-estado  fa fa-check-square" aria-hidden="true"></i>
+                            <input class="form" id="usu_nom" type="text" name="usu_nom"  placeholder="Ingrese el nombre ejemplo maria" required/>
+                            <span>(required, at least 3 characters)</span>
                         </div>
                     </div>
 
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Apellido Paterno: <span class="tx-danger">*</span></label>
-                            <input class="form" id="usu_apep" type="text" name="usu_apep" placeholder="Ingrese el apellido ejemplo hernandez"/>
+                            <input class="form" id="usu_apep" type="text" name="usu_apep" placeholder="Ingrese el apellido ejemplo hernandez"required/>
                         </div>
                     </div>
 
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Apellido Materno: <span class="tx-danger">*</span></label>
-                            <input class="form" id="usu_apem" type="text" name="usu_apem"placeholder="Ingrese el appelido materno ejemplo guzaman" />
+                            <input class="form" id="usu_apem" type="text" name="usu_apem"placeholder="Ingrese el appelido materno ejemplo guzaman"required />
                         </div>
                     </div>
 
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Correo: <span class="tx-danger">*</span></label>
-                            <input class="form" id="usu_correo" type="email" name="usu_correo" placeholder="Ingrese el correo electronico ejemplo maria@homail.com" />
+                            <input class="form" id="usu_correo" type="email" name="usu_correo" placeholder="Ingrese el correo electronico ejemplo maria@homail.com" required/>
                         </div>
                     </div>
 
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Password: <span class="tx-danger">*</span></label>
-                            <input class="form" id="usu_pass" type="password" name="usu_pass"placeholder="Ingrese el password" />
+                            <input class="form" id="usu_pass" type="password" name="usu_pass"placeholder="Ingrese el password" required/>
                         </div>
                     </div>
 
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Sexo: <span class="tx-danger">*</span></label>
-                            <select class="form select2" style="width:100%" name="usu_sex" id="usu_sex" data-placeholder="Seleccione">
+                            <select class="form select2" style="width:100%" name="usu_sex" id="usu_sex" data-placeholder="Seleccione" required>
                                 <option label="Seleccione"></option>
                                 <option value="M">Masculino</option>
                                 <option value="F">Femenino</option>
@@ -59,14 +59,14 @@
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Telefono: <span class="tx-danger">*</span></label>
-                            <input class="form " id="usu_telf" type="text" name="usu_telf" placeholder="Ingrese el telefono ejemplo 932115475" />
+                            <input class="form " id="usu_telf" type="text" name="usu_telf" placeholder="Ingrese el telefono ejemplo 932115475"required />
                         </div>
                     </div>
 
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Rol: <span class="tx-danger">*</span></label>
-                            <select class="form select2" style="width:100%" name="rol_id" id="rol_id" data-placeholder="Seleccione">
+                            <select class="form select2" style="width:100%" name="rol_id" id="rol_id" data-placeholder="Seleccione" required>
                                 <option label="Seleccione"></option>
                                 <option value="1">Usuario</option>
                                 <option value="2">Admin</option>
@@ -77,7 +77,7 @@
                     <div class="col-lg-12">
                         <div class="form-group">
                             <label class="form-control-label">Matricula: <span class="tx-danger">*</span></label>
-                            <input class="form" id="usu_matri" type="text" name="usu_matri"placeholder="Ingrese la matricula 19E478"/>
+                            <input class="form" id="usu_matri" type="text" name="usu_matri"placeholder="Ingrese la matricula 19E478" required>
                         </div>
                     </div>
 
